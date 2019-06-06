720663e60f5ea1f66137ed0c57b16f93c892a224
Nitin Chauhan <nitin.chauhan@gridbeyond.com>
2019-06-06 12:30:58 +0100

    R Script

--- Titanic.R
+++ Titanic.R
@@ -2,74 +2,122 @@
 #~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Ingesting & Preprocessing Datasets ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 ###############################################################################
 rm(list=ls(all=TRUE))
+gc()
+
 #Current working directory
 getwd()
-#Set working directory to where your data files are stored
-setwd("C:/Users/Desktop/Titanic Machine Learning") 
+
+# Step 1: Set working directory to where your data files are stored
+setwd("C:\\Users\\Nitin.Chauhan\\Documents\\GitHub\\Kaggle-Titanic-MachineLearning")
 library(ggplot2)
 library(ggrepel)
 
-#Import the data and read the file
+# Step 2: Import the data and read the file
 traindata <- read.csv(file="train.csv", header=TRUE,na.strings=c(""))
 testdata <- read.csv(file="test.csv", header = TRUE, na.strings=c(""))
+# Save the Survived labels from train data in variable survived
 survived <- traindata$Survived
-combined_data <- rbind(traindata[,-2],testdata)
 
-#Determine the class and structure of train dataset
-class(combined_data)
-str(combined_data)
-sapply(combined_data, function(x) sum(is.na(x)))
+# Step 3: Determine the class and structure of train dataset
+class(traindata)
+sapply(traindata, function(x) sum(is.na(x)))
+# Preprocessing the Train Dataset
+traindata$Pclass <- as.factor(traindata$Pclass)
+traindata$Age <- as.integer(traindata$Age)
+traindata$Name <- as.character(traindata$Name)
+str(traindata)
 
-#Preprocessing the Dataset
-combined_data$Pclass <- as.factor(combined_data$Pclass)
-combined_data$Age <- as.integer(combined_data$Age)
-combined_data$Name <- as.character(combined_data$Name)
-str(combined_data)
+# Step 4: Determine the class and structure of test dataset
+class(testdata)
+sapply(testdata, function(x) sum(is.na(x)))
+# Preprocessing the Test Dataset
+testdata$Pclass <- as.factor(testdata$Pclass)
+testdata$Age <- as.integer(testdata$Age)
+testdata$Name <- as.character(testdata$Name)
+str(testdata)
 
-#Feature Engineering
-combined_data$Title <- gsub('(.*, )|(\\..*)', '', combined_data$Name)
-combined_data$Title <- as.factor(combined_data$Title)
-str(combined_data$Title)
+# Step 5: Feature Engineering in Train Data
+# Create a new feature Title
+traindata$Title <- gsub('(.*, )|(\\..*)', '', traindata$Name)
+traindata$Title <- as.factor(traindata$Title)
+str(traindata$Title)
+# Create a new feature family_size
+traindata$family_size <- as.integer(traindata$SibSp + traindata$Parch + 1)
 
-combined_data$family_size <- as.integer(combined_data$SibSp+combined_data$Parch+1)
-#Drop Cabin since more than 70% values are missing
-#Drop some of the predictors that are redundant
-combined_data <- subset(combined_data,select = -c(Name,SibSp,Parch,Ticket,Cabin))
-combined_data$Fare <- as.integer(combined_data$Fare)
-combined_data[which(combined_data$Age==0),] #None title as Mr, Mrs, or above 18
-combined_data$Age[which(combined_data$Age==0)] <- 1
+"Drop Cabin since more than 70% values are missing
+Drop some of the predictors that are redundant"
+traindata <- subset(traindata,select = -c(Name,SibSp,Parch,Ticket,Cabin))
 
-levels(combined_data$Title)
-sort(summary(combined_data$Title)[1:18],decreasing = T)
-var <- c("Mr","Miss","Mrs","Master")
-length(var);rev(var)
+# Identify if any Age is identified as 0
+traindata[which(traindata$Age==0),] # Where Age is less than a year either new born or wrongly interpreted
+traindata$Age[which(traindata$Age==0)] <- 1 # Since Title are either Master or Miss
 
-for(i in rev(var)){
-  combined_data$Title <- relevel(combined_data$Title, i)
-}
-#Grouping the remaining speakers as 'others'
-levels(combined_data$Title) <- c("Mr","Miss","Mrs","Master", rep("VIP",14))
-qplot(combined_data$Title)
+levels(traindata$Title)
+sort(summary(traindata$Title)[1:17],decreasing = T)
+# Create a new variable var holding few titles
+var <- c("Mr", "Mrs", "Master", "Miss", "Dr")
+# Since there are some titles that are 1 in count causing the system to see some unseen title in test data
+length(traindata$Title[!traindata$Title %in% var]) # 20
+# Rename the 20 other titles as VIP
+traindata$Title <- as.character(traindata$Title)
+traindata$Title[!traindata$Title %in% var] <- 'VIP'
+traindata$Title <- as.factor(traindata$Title)
 
+# Step 6: Feature Engineering in Test Data
+# Create a new feature Title
+testdata$Title <- gsub('(.*, )|(\\..*)', '', testdata$Name)
+testdata$Title <- as.factor(testdata$Title)
+str(testdata$Title)
+# Create a new feature family_size
+testdata$family_size <- as.integer(testdata$SibSp + testdata$Parch + 1)
 
-#Imputation of missing values
-#Handle NAs in the dataset
-sapply(combined_data,function(x) sum(is.na(x)))
-#Use the concept of Random Forest to calculate missing values for quantitative
-#and qualitative predictors. It's a non-parametric approach without any assumptions
+"Drop Cabin since more than 70% values are missing
+Drop some of the predictors that are redundant"
+testdata <- subset(testdata,select = -c(Name,SibSp,Parch,Ticket,Cabin))
+
+# Identify if any Age is identified as 0
+testdata[which(testdata$Age==0),] # Where Age is less than a year either new born or wrongly interpreted
+testdata$Age[which(testdata$Age==0)] <- 1 # Since Title are either Master or Miss
+
+levels(testdata$Title)
+sort(summary(testdata$Title)[1:9],decreasing = T)
+# Since there are some titles that are 1 in count causing the system to see some unseen title in test data
+length(testdata$Title[!testdata$Title %in% var]) # 6 
+# Rename the 6 other titles as VIP
+testdata$Title <- as.character(testdata$Title)
+testdata$Title[!testdata$Title %in% var] <- 'VIP'
+testdata$Title <- as.factor(testdata$Title)
+
+"Use the concept of Random Forest to calculate missing values for quantitative
+and qualitative predictors. It's a non-parametric approach without any assumptions"
 #install.packages("missForest")
 library(missForest)
-combined_data.imp <- missForest(combined_data)
-sum(is.na(combined_data.imp$ximp))
-#Shows the error rate during computation of missing values
-#Quite an effective way to check quality of computational process
-combined_data.imp$OOBerror
-combined_data <- combined_data.imp$ximp
-str(combined_data)
 
-traindata <- combined_data[1:891,]
-traindata$Survived <- survived
-testdata <- combined_data[892:1309,]
+# Step 7: Impute Missing Values in Train Data
+sapply(traindata,function(x) sum(is.na(x)))
+traindata.imp <- missForest(traindata)
+sum(is.na(traindata.imp$ximp))
+"Shows the error rate during computation of missing values
+Quite an effective way to check quality of computational process"
+traindata.imp$OOBerror
+traindata <- traindata.imp$ximp
+
+# Step 8: Impute Missing Values in Test Data
+sapply(testdata,function(x) sum(is.na(x)))
+testdata.imp <- missForest(testdata)
+sum(is.na(testdata.imp$ximp))
+"Shows the error rate during computation of missing values
+Quite an effective way to check quality of computational process"
+testdata.imp$OOBerror
+testdata <- testdata.imp$ximp
+
+
+# Step 9: Identify number of observation for each class
+count_survived <- qplot(factor(traindata$Survived), fill = factor(traindata$Survived))
+count_survived + geom_label_repel(stat='count', aes(label=..count..), nudge_x = 0.0, nudge_y = -50)
+"Since the labels are imbalanced in count, We will use Precision-Recall curves 
+as it summarize the trade-off between the true positive rate and 
+the positive predictive value for a predictive model using different probability thresholds."
 
 ###############################################################################
 #~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Visualizing the Datasets ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
@@ -109,199 +157,136 @@ ggplot(traindata, aes(x= Title, fill=factor(Pclass))) +
   ylab("Total Count") + 
   labs(fill="Pclass")
 
+
+#Splitting the dataset into 70:30 Train and Valid
+size_train <- nrow(traindata)
+sample_index <- sample.int(size_train, size = floor(0.3*nrow(traindata)))
+valid <- traindata[sample_index,]
+train <- traindata[-sample_index,]
+
+
 ###############################################################################
 #~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ Logistic Regression ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 ###############################################################################
-#Splitting the dataset into 80:20 Train and Test.
-size_train <- nrow(traindata)
-sample_index <- sample.int(size_train, size = floor(0.2*nrow(traindata)))
-testData <- traindata[sample_index,]
-trainData <- traindata[-sample_index,]
-
 library(e1071)
 #To verify that the model is reasonable, we calculate the data fitting for 
 #both training set and cross validation set.
-logmodel <- glm(factor(Survived)~., data = testData, family = binomial(link = logit), control = list(maxit=15,epsilon=1e-4))
+logmodel <- glm(factor(Survived)~., data = valid, family = binomial(link = logit), control = list(maxit=15,epsilon=1e-4))
 cutoffs <- seq(0.1,0.9,0.1)
 accuracy <- NULL
 
 #Function to Set accuracy for each cutoffs
 for (i in seq(along = cutoffs)){
   prediction <- ifelse(logmodel$fitted.values >= cutoffs[i], 1, 0) #Predicting for cut-off
-  accuracy <- c(accuracy,length(which(factor(testData$Survived) ==prediction))/length(prediction)*100)
+  accuracy <- c(accuracy,length(which(factor(valid$Survived) ==prediction))/length(prediction)*100)
 }
 
 plot(cutoffs, accuracy, pch =19,type='b',col= "steelblue",
      main ="Logistic Regression", xlab="Cutoff Level", ylab = "Accuracy %")
 
-#From the plot above the best Accuracy % is at cutoff 0.50
+#From the plot above the best Accuracy % is at cutoff 0.6
 
-full_mod <- glm(factor(Survived)~., data = trainData, family = binomial(link = logit), control = list(maxit=15,epsilon=1e-4))
+full_mod <- glm(factor(Survived)~., data = train, family = binomial(link = logit), control = list(maxit=15,epsilon=1e-4))
 summary(full_mod)
 library(MASS)
 backward <- stepAIC(full_mod,direction="backward",trace=FALSE)
 #Building the model with lowest deviance and lowest AIC
 mylogit <- glm(formula = factor(Survived) ~ Pclass + Sex + Age + Fare + 
-                 Title + family_size, family = binomial(link = logit), data = trainData, control = list(maxit = 15, epsilon = 1e-04))
+                 Title + family_size, family = binomial(link = logit), data = train, 
+               control = list(maxit = 15, epsilon = 1e-04))
 
 summary(mylogit)
 
-predSurv <- predict.glm(mylogit,testdata)
-predSurv <-ifelse(predSurv > 0.5,1,0)
+# Test the model on valid
+pred <- predict.glm(mylogit,valid)
+pred <- ifelse(pred > 0.5,1,0)
+nrow(valid)
+length(pred)
+library(caret)
+xtab <- table(pred, valid$Survived)
+print(confusionMatrix(xtab[2:1,2:1]))
 
+# Prediction on TestData
+predSurv <- predict.glm(mylogit,testdata)
+predSurv <- ifelse(predSurv > 0.5,1,0)
+length(predSurv)
 final_result <- data.frame(testdata$PassengerId,predSurv)
 colnames(final_result) <- c("PassengerId","Survived")
 write.csv(final_result, file="logistic.csv", row.names=FALSE)
 
-
 ###############################################################
 #----------------------------- SVM ---------------------------#
 ###############################################################
-str(combined_data)
-SVMmodel<-svm(factor(Survived) ~ Pclass + Sex + Age + Fare + 
-                Title + family_size, data = traindata, kernel="radial", cost = 100, gamma = 1)
+# Step 1: Compare Kernels based on Validation set
+# Step 2: Radial Kernel
+SVMmodel<-svm(factor(Survived) ~ ., data = valid, kernel="radial", cost = 100, gamma = 1)
 print(SVMmodel)
 summary(SVMmodel)
-compareTable <- table(predict(SVMmodel),traindata$Survived)  
-mean(traindata$Survived != predict(SVMmodel)) #10.7% misclassification error
+compareTable <- table(predict(SVMmodel),valid$Survived)  
+mean(valid$Survived != predict(SVMmodel)) #1.12% misclassification error
 
-SVMmodel<-svm(factor(Survived) ~ Pclass + Sex + Age + Fare + 
-                Title + family_size, data = traindata, kernel="linear", cost = 100, gamma = 1)
+# Step 3: Linear Kernel
+SVMmodel<-svm(factor(Survived) ~ ., data = valid, kernel="linear", cost = 100, gamma = 1)
 print(SVMmodel)
 summary(SVMmodel)
-compareTable <- table(predict(SVMmodel),traindata$Survived)  
-mean(traindata$Survived != predict(SVMmodel)) #16.8% misclassification error
-# Tuning
-# Prepare training and test data
-set.seed(100) # for reproducing results
-sum(is.na(traindata))
-rowIndices <- 1 : nrow(traindata) # prepare row indices i.e. 891
-sampleSize <- 0.7 * length(rowIndices) # training sample size i.e 623 = 0.7*891
-trainingRows <- sample (rowIndices, sampleSize) # random sampling of 623 rows
-trainingData <- traindata[trainingRows, ] # training data i.e. random sample 623 values
-testData <- traindata[-trainingRows, ] # test data i.e. rando sample 267 values
+compareTable <- table(predict(SVMmodel),valid$Survived)
+mean(valid$Survived != predict(SVMmodel)) #16.47% misclassification error
 
-tuned <- tune.svm(factor(Survived) ~ Pclass + Sex + Age + Fare + 
-                    Title + family_size, data = traindata, gamma = 10^(-6:-1), cost = 10^(1:3)) # tune
-
+"The Radial kernel seems to result in low misclassification error thus we will use it for further modeling"
+# Step 4: Tune the model using tune.svm on validation set
+tuned <- tune.svm(factor(Survived) ~ ., data = valid, gamma = 10^(-6:-1), cost = 10^(1:3)) # tune
 summary (tuned) # to select best gamma and cost
+model1 <- svm (factor(Survived) ~., data = valid, kernel = "radial", cost = 100, gamma=0.01) # radial svm, scaling turned OFF
+print(model1)
+compareTable <- table (valid$Survived, predict(model1, valid))  # comparison table
+mean(valid$Survived != predict(model1, valid)) # 13.48% misclassification train error
 
-svmfit <- svm (factor(Survived) ~., data = traindata, kernel = "radial", cost = 100, gamma=0.01) # radial svm, scaling turned OFF
+# Step 5: Tune the model using k fold cross validation
+train_control <- trainControl(method="cv", number=10)
+# Fit SVM Radial Model
+model <- train(factor(Survived) ~ ., data=valid, trControl=train_control, method="svmRadial")
+# Summarise Results
+print(model)
+# C=0.50  Accuracy=0.8390313  K=0.6360905
+
+# Step 6: Create a prediction model
+svmfit <- train(factor(Survived) ~ ., data=train, trControl=train_control, method="svmRadial")
+# Summarise Results
 print(svmfit)
-compareTable <- table (testData$Survived, predict(svmfit, testData))  # comparison table
-mean(testData$Survived != predict(svmfit, testData)) # 14.55% misclassification train error
-1-0.1455 #85.45% Accuracy
-#Testing on unseen data = testdata
+
+# Step 7: Testing on unseen data 'testdata'
 prediction1 <- predict(svmfit, testdata)
+length(prediction1)
 final_result <- data.frame(testdata$PassengerId,prediction1)
 colnames(final_result) <- c("PassengerId","Survived")
 write.csv(final_result, file="svm.csv", row.names=FALSE)
-
-###############################################################
-#--------------------------- Decision Tree -------------------#
-###############################################################
-#Predict Survival (classify)
-dim(traindata)
-train_data <- traindata[,-c(1)]
-str(train_data)
-
-dim(testdata)
-PassengerId <- testdata[,1]
-test_data <- testdata[,-c(1)]
-str(test_data)
-
-train_data$Survived <- as.factor(train_data$Survived)
-
-#C5.0 good classification technique but does not 
-#provide good plots. For that rpart is good
-library(C50)
-Model_C50 <-C5.0(train_data[,-8],train_data[,8]) #Without Survived, Survived
-#Here 8 is column for 'Survived'
-Model_C50
-summary(Model_C50)
-
-#Predicting on Train
-P1_train=predict(Model_C50,train_data);P1_train
-table(train_data[,8],Predicted=P1_train)    #Here 8 is column for 'Survived'
-(516+251)/(516+33+91+251)
-#86.08
-
-#Predicting on Test
-P1_test = predict(Model_C50,test_data);P1_test
-final_result <- data.frame(PassengerId,P1_test)
-colnames(final_result) <- c("PassengerId","Survived")
-write.csv(final_result, file="MC50.csv", row.names=FALSE)
-
-#Rpart
-library(rpart)
-library(rpart.plot)
-
-Model_rpart= rpart(factor(Survived)~.,data=train_data, method="class")
-Model_rpart
-summary(Model_rpart)
-#plot(Model_rpart)
-#rpart.plot(Model_rpart,type=3,extra=101,fallen.leaves = FALSE)
-
-#Predicting on Train
-P1_train_rpart=predict(Model_rpart,train_data,type="class")
-table(train_data[,8],predicted=P1_train_rpart)  #Here 8 is column for 'Survived'
-(477+251)/(477+72+91+251) # 81.70
-
-#Predicting on Test
-P1_test_rpart=predict(Model_rpart,test_data,type="class")
-final_result <- data.frame(PassengerId, P1_test_rpart)
-names(final_result) <- c("PassengerId","Survived")
-write.csv(final_result,file = "Rpart.csv", row.names = FALSE)
-
 
 
 ###############################################################
 #--------------------- RANDOM FOREST -------------------------#
 ###############################################################
-#Grid Search for Tuning Hyperparameters using CARET
-library(caret)
+
+# Step 1: Compare Different Tuning algorithms based on Validation set
+# Step 2: Grid Search for Tuning Hyperparameters using CARET
 library(randomForest)
-y <- as.factor(survived)
-bestmtry <- tuneRF(traindata,y, stepFactor=1.5, improve=1e-5, ntreeTry = 500)
+y <- as.factor(valid$Survived)
+bestmtry <- tuneRF(valid[,-2],y, stepFactor=1.5, improve=1e-5, ntreeTry = 500)
 print(bestmtry)
-rf1 <- randomForest(factor(survived) ~ ., data=traindata[-1], keep.forest=TRUE, ntree=500, mtry=2)
-print(rf1) #OOB estimate of  error rate: 16.72%
+rf1 <- randomForest(factor(train$Survived) ~ ., data=train[-2], keep.forest=TRUE, ntree=500, mtry=2)
+print(rf1) # OOB estimate of  error rate: 16.72%
 varImpPlot(rf1)
-#confusion Matrix
-table(survived, predict(rf1, traindata[,-1], type="response", norm.votes=TRUE))
-RFpred <- predict(rf1, testdata[,-1], type="response", norm.votes=TRUE)
+# confusion Matrix
+table(train$Survived, predict(rf1, train[,-2], type="response", norm.votes=TRUE))
+
+# Step 3: Tune the model using k fold cross validation
+train_control <- trainControl(method="cv", number=10)
+# Fit SVM Radial Model
+rf2 <- train(factor(Survived) ~ ., data=train, trControl=train_control, method="rf")
+# Summarise Results
+print(rf2)
+
+# Step 4: Testing on unseen data 'testdata'
+RFpred <- predict(rf2, testdata)
 final_result <- data.frame(testdata$PassengerId, RFpred)
 names(final_result) <- c("PassengerId","Survived")
 write.csv(final_result,file = "RF.csv", row.names = FALSE)
-
-###############################################################
-#------------------------- XG BOOOST -------------------------#
-###############################################################
-str(traindata)
-str(testdata)
-library(xgboost)
-library(mlr)
-class(traindata$Pclass)
-traindata$Pclass <- as.numeric(as.character(traindata$Pclass))
-traindata$Title <- createDummyFeatures(traindata$Title)
-traindata$Embarked <- createDummyFeatures(traindata$Embarked)
-traindata$Sex <- createDummyFeatures(traindata$Sex)
-
-testdata$Pclass <- as.numeric(as.character(testdata$Pclass))
-testdata$Title <- createDummyFeatures(testdata$Title)
-testdata$Embarked <- createDummyFeatures(testdata$Embarked)
-testdata$Sex <- createDummyFeatures(testdata$Sex)
-
-
-bst <- xgboost(data=as.matrix(traindata),
-               label = survived, max_depth=5, eta=0.5, 
-               nthread=3, nrounds = 4, eval_metric = "error", 
-               objective="binary:logistic", verbose = 1)
-pred <- predict(bst,as.matrix(testdata))
-print(length(pred))
-print(head(pred))
-prediction <- as.numeric(pred>0.5)
-print(head(prediction))
-final_result <- data.frame(testdata$PassengerId, prediction)
-names(final_result) <- c("PassengerId","Survived")
-write.csv(final_result,file = "XGB.csv", row.names = FALSE)
